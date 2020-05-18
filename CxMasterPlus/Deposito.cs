2fb5fff54fd5fe9606a808ddc0f3d061da40f03e
Marcos Carvalho <bcmarcos93@gmail.com>
2020-05-17 23:40:45 -0300

    correção na validação de depósito.

--- CxMasterPlus/Deposito.cs
+++ CxMasterPlus/Deposito.cs
@@ -5,17 +5,26 @@ using System.Text;
 namespace CxMasterPlus
 {
     public class Deposito
-    { 
+    {
         public String realizarDeposito(int nrConta, BaseDeDados baseDeDados, CompartimentoDeSaque compartimentoDeSaque, int tipoDeposito, int qtd)
         {
             try
             {
-                baseDeDados.creditarValor(nrConta, qtd);
-                if (tipoDeposito != 1)
+                int tipoConta = baseDeDados.getTipoConta(nrConta);
+                if (tipoConta == 1 && tipoDeposito == 1)
                 {
-                    compartimentoDeSaque.adicionarNotas(qtd);
+                    return "Contas universitárias não podem efetuar depósito em cheque.";
                 }
-                return "Transação Efetivada.";
+                if (tipoDeposito == 1 || tipoDeposito == 2)
+                {
+                    baseDeDados.creditarValor(nrConta, qtd);
+                    if (tipoDeposito == 2)
+                    {
+                        compartimentoDeSaque.adicionarNotas(qtd);
+                    }
+                    return "Transação Efetivada.";
+                }
+                return "Problemas ao efetuar sua transação. Tente novamente mais tarde.";
             }
             catch (Exception)
             {
