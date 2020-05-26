3769b4a8904bd95910f3f36679132fd1b47407e4
edugriggio <edu_griggio@hotmail.com>
2020-05-25 22:27:43 -0300

    Add files via upload

---
+++ CxMasterPlus/Validadores.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace CxMasterPlus
+{
+    public class Validadores
+    {
+
+        private bool usuarioAutenticado = false;
+        private int tentativas = 3;
+        private int senha;
+        public Boolean ValidarUsuario(Tela tela, int conta, BaseDeDados baseDeDados)
+        {
+            while (!usuarioAutenticado && tentativas > 0)
+            {
+                tela.ImprimirMensagem("Por favor, digite sua senha: ");
+                try
+                {
+                    senha = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    senha = 0;
+                }
+                usuarioAutenticado = baseDeDados.AutenticarUsuario(conta, senha);
+                tentativas--;
+                if (!usuarioAutenticado && tentativas > 0)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Senha Inválida. Você possui mais " + tentativas + " tentativas.");
+                }
+                else if (!usuarioAutenticado && tentativas == 0)
+                {
+                    Console.Clear();
+                    return false;
+                }
+            }
+            Console.Clear();
+            return usuarioAutenticado;
+        }
+
+        public int ValidarInputMenu(Tela tela, string input)
+        {
+            int testaInput = 0;
+
+            try
+            {
+                testaInput = Convert.ToInt32(input);
+            }
+            catch (Exception)
+            {
+                Console.Clear();
+                tela.ImprimirMensagem("O valor digitado é inválido. Sua Transação será cancelada.");
+                Console.ReadKey();
+                Console.Clear();
+                return 0;
+            }
+
+            return testaInput;
+        }
+    }
+}
