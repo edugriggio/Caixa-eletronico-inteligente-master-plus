501015e82d61f00e368c4049b3b9639d465fc244
Marcos Carvalho <bcmarcos93@gmail.com>
2020-05-26 22:38:14 -0300

    correção na validação de data.

--- CxMasterPlus/BackEnd/Saque.cs
+++ CxMasterPlus/BackEnd/Saque.cs
@@ -4,7 +4,6 @@ namespace CxMasterPlus
 {
     public class Saque
     {
-        private readonly DateTime date = new DateTime();
 
         public String EfetuarSaque(int nrConta, BaseDeDados baseDeDados, int qtd, CompartimentoDeSaque compartimentoDeSaque)
         {
@@ -12,7 +11,7 @@ namespace CxMasterPlus
             {
                 double saldoDisponivel;
 
-                if (baseDeDados.getLimiteDiario(nrConta, (date.DayOfWeek - 1).ToString()) == 0)
+                if (baseDeDados.getLimiteDiario(nrConta, (DateTime.Today.DayOfWeek).ToString()) == 0)
                 {
                     return "Valor excede o limite diário conforme o seu tipo de conta.";
                 }
@@ -27,12 +26,12 @@ namespace CxMasterPlus
                 {
                     if (compartimentoDeSaque.TemSaldoSuficiente(qtd))
                     {
-                        if (baseDeDados.getLimiteDiario(nrConta, (date.DayOfWeek - 1).ToString()) >= qtd)
+                        if (baseDeDados.getLimiteDiario(nrConta, (DateTime.Today.DayOfWeek).ToString()) >= qtd)
                         {
                             baseDeDados.DebitarValor(nrConta, qtd);
 
                             compartimentoDeSaque.DispensarDinheiro(qtd);
-                            baseDeDados.SubtrairLimiteDiario(nrConta, qtd, (date.DayOfWeek - 1).ToString());
+                            baseDeDados.SubtrairLimiteDiario(nrConta, qtd, (DateTime.Today.DayOfWeek).ToString());
                             return "Transação realizada.\nPor favor, retire seu dinheiro.";
                         }
                         else
