b50f358dd2cb61a6e5ab1fc3520bdc3b0b2733c0
Rafael <Rafael@DESKTOP-T3GKF5G>
2020-05-28 18:38:21 -0300

    Atualização dos comentarios.

--- BancoDeTestes/SaqueTeste.cs
+++ BancoDeTestes/SaqueTeste.cs
@@ -1,8 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using CxMasterPlus;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using System.Text;
 
 namespace BancoDeTestes
 {
@@ -18,7 +15,8 @@ namespace BancoDeTestes
         {
             Saque saque = new Saque();
             string teste;
-            // teste dos limites diarios
+
+            //teste dos limites diarios
             Assert.AreEqual(200, dados.getLimiteDiario(9999, "Sunday"));
             Assert.AreEqual(200, dados.getLimiteDiario(9999, "Saturday"));
             Assert.AreEqual(200, dados.getLimiteDiario(9999, "Monday"));
@@ -51,13 +49,13 @@ namespace BancoDeTestes
 
         }
 
-         [TestMethod]
+        [TestMethod]
          public void Testesaquecontapadrao()
          {
              Saque saque = new Saque();
              string teste;
-
-             Assert.AreEqual(750, dados.getLimiteDiario(8888, "Sunday"));
+            //teste dos limites diarios
+            Assert.AreEqual(750, dados.getLimiteDiario(8888, "Sunday"));
              Assert.AreEqual(750, dados.getLimiteDiario(8888, "Saturday"));
              Assert.AreEqual(1000, dados.getLimiteDiario(8888, "Monday"));
              Assert.AreEqual(1000, dados.getLimiteDiario(8888, "Tuesday"));
@@ -65,21 +63,25 @@ namespace BancoDeTestes
              Assert.AreEqual(1000, dados.getLimiteDiario(8888, "Thursday"));
              Assert.AreEqual(1000, dados.getLimiteDiario(8888, "Friday"));
 
-             teste = saque.EfetuarSaque(8888, dados, -200, compartimentoDeSaque);
-             Assert.AreEqual("Valor inválido", teste);
+            //teste de saque de valor negativo
+            teste = saque.EfetuarSaque(8888, dados, -200, compartimentoDeSaque);
+            Assert.AreEqual("Valor inválido", teste);
 
-             teste = saque.EfetuarSaque(8888, dados, 0, compartimentoDeSaque);
-             Assert.AreEqual("Valor inválido", teste);
+            //teste de saque de zero
+            teste = saque.EfetuarSaque(8888, dados, 0, compartimentoDeSaque);
+            Assert.AreEqual("Valor inválido", teste);
 
-             teste = saque.EfetuarSaque(8888, dados, 2000, compartimentoDeSaque);
-             Assert.AreEqual("Você não possui saldo suficiente em sua conta.\n\nPor favor, escolha um valor menor.", teste);
+            //teste de saque de valor superior ao limite diario
+            teste = saque.EfetuarSaque(8888, dados, 2000, compartimentoDeSaque);
+            Assert.AreEqual("Você não possui saldo suficiente em sua conta.\n\nPor favor, escolha um valor menor.", teste);
 
+            //teste de saque do dentro do limite diario
+            teste = saque.EfetuarSaque(8888, dados, 200, compartimentoDeSaque);
+            Assert.AreEqual("Transação realizada.\nPor favor, retire seu dinheiro.", teste);
 
-             teste = saque.EfetuarSaque(8888, dados, 200, compartimentoDeSaque);
-             Assert.AreEqual("Transação realizada.\nPor favor, retire seu dinheiro.", teste);
-
-             teste = saque.EfetuarSaque(8888, dados, 600, compartimentoDeSaque);
-             Assert.AreEqual("Valor ultrapassa seu limite de saque diário.", teste);
+            //teste de saque apos ultrapassar o limite diario
+            teste = saque.EfetuarSaque(8888, dados, 900, compartimentoDeSaque);
+            Assert.AreEqual("Valor ultrapassa seu limite de saque diário.", teste);
          }
 
          [TestMethod]
@@ -88,6 +90,7 @@ namespace BancoDeTestes
              Saque saque = new Saque();
              string teste;
 
+             //teste dos limites diarios
              Assert.AreEqual(5000, dados.getLimiteDiario(7777, "Sunday"));
              Assert.AreEqual(5000, dados.getLimiteDiario(7777, "Saturday"));
              Assert.AreEqual(3000, dados.getLimiteDiario(7777, "Monday"));
@@ -96,20 +99,25 @@ namespace BancoDeTestes
              Assert.AreEqual(3000, dados.getLimiteDiario(7777, "Thursday"));
              Assert.AreEqual(3000, dados.getLimiteDiario(7777, "Friday"));
 
-             teste = saque.EfetuarSaque(7777, dados, -200, compartimentoDeSaque);
-             Assert.AreEqual("Valor inválido", teste);
+            //teste de saque de valor negativo
+            teste = saque.EfetuarSaque(7777, dados, -200, compartimentoDeSaque);
+            Assert.AreEqual("Valor inválido", teste);
 
-             teste = saque.EfetuarSaque(7777, dados, 0, compartimentoDeSaque);
-             Assert.AreEqual("Valor inválido", teste);
+            //teste de saque de zero
+            teste = saque.EfetuarSaque(7777, dados, 0, compartimentoDeSaque);
+            Assert.AreEqual("Valor inválido", teste);
 
-             teste = saque.EfetuarSaque(7777, dados, 6000, compartimentoDeSaque);
-             Assert.AreEqual("Você não possui saldo suficiente em sua conta.\n\nPor favor, escolha um valor menor.", teste);
+            //teste de saque de valor superior ao limite diario
+            teste = saque.EfetuarSaque(7777, dados, 6000, compartimentoDeSaque);
+            Assert.AreEqual("Você não possui saldo suficiente em sua conta.\n\nPor favor, escolha um valor menor.", teste);
 
-             teste = saque.EfetuarSaque(7777, dados, 4000, compartimentoDeSaque);
-             Assert.AreEqual("Transação realizada.\nPor favor, retire seu dinheiro.", teste);
+            //teste de saque do dentro do limite diario
+            teste = saque.EfetuarSaque(7777, dados, 3000, compartimentoDeSaque);
+            Assert.AreEqual("Transação realizada.\nPor favor, retire seu dinheiro.", teste);
 
-             teste = saque.EfetuarSaque(7777, dados, 1500, compartimentoDeSaque);
-             Assert.AreEqual("Valor ultrapassa seu limite de saque diário.", teste);
+            //teste de saque apos ultrapassar o limite diario
+            teste = saque.EfetuarSaque(7777, dados, 3000, compartimentoDeSaque);
+            Assert.AreEqual("Valor excede o limite diário conforme o seu tipo de conta.", teste);
          }
     }
 }
